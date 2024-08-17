40ecb578f63877b66ba467766ef6b1182ca85c19
skylotus-design <daniswaradadan4@gmail.com>
2024-08-17 19:54:24 +0700

    20240817

---
+++ docs/download_div/div_to_image.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    <div id="myDiv" style="width: 200px; height: 200px; background-color: lightblue;">
+        <h1>Hello, World!</h1>
+    </div>
+    <button id="downloadBtn">Download as Image</button>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dom-to-image/2.6.0/dom-to-image.min.js"></script>
+    <script>
+        document.getElementById('downloadBtn').addEventListener('click', function() {
+            domtoimage.toBlob(document.getElementById('myDiv'))
+                .then(function (blob) {
+                    var link = document.createElement('a');
+                    link.href = URL.createObjectURL(blob);
+                    link.download = 'myDiv.png';
+                    link.click();
+                });
+        });
+    </script>
+</body>
+</html>
---
+++ docs/drag_image/halaman.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload and Drag Image</title>
+    <style>
+        #canvas {
+            border: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+    <input type="file" id="upload" accept="image/*">
+    <canvas id="canvas" width="800" height="600"></canvas>
+
+    <script src="script.js"></script>
+</body>
+</html>
